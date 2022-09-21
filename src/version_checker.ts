45eace9607b0544072f5d05fdc4aa55ff30afaa4
Marten Lohstroh <marten@berkeley.edu>
2022-09-20 23:10:12 -0700

    Allow versions of Java greater than 17

--- src/version_checker.ts
+++ src/version_checker.ts
@@ -61,8 +61,8 @@ const theBetterOfEither: VersionCheckerCombiner = (check0, check1) => async () =
     return zeroth;
 };
 
-export const javaVersionChecker = basicChecker(config.javaVersion, 'java -version 2>&1', true);
-export const javacVersionChecker = basicChecker(config.javacVersion, 'javac -version 2>&1', true);
+export const javaVersionChecker = basicChecker(config.javaVersion, 'java -version 2>&1', false);
+export const javacVersionChecker = basicChecker(config.javacVersion, 'javac -version 2>&1', false);
 export const python3AliasVersionChecker = basicChecker(config.pythonVersion, 'python3 -V', false);
 export const pythonAliasVersionChecker = basicChecker(config.pythonVersion, 'python -V', false);
 export const python3VersionChecker = theBetterOfEither(
