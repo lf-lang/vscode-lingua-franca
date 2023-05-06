3e4b7a36e07fb233a11869a2465233fe12e21fb4
Peter Donovan <peterdonovan@berkeley.edu>
2023-05-05 19:16:10 -0700

    Double the extended timeout.

--- src/test/check_dependencies.test.ts
+++ src/test/check_dependencies.test.ts
@@ -29,7 +29,7 @@ enum DependencyStatus {
 type Test = () => Promise<void>;
 
 const basicTimeoutMilliseconds = 20 * 1000;
-const extendedTimeoutMilliseconds = 60 * 1000;
+const extendedTimeoutMilliseconds = 120 * 1000;
 const maxInstallationTimeMilliseconds = 30 * 1000;
 const linkCheckingTimeoutMilliseconds = 60 * 1000;
 
