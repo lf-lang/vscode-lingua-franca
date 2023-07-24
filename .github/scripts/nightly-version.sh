f8b80045759f29e442a9cd10c035eeea2740a825
Marten Lohstroh <marten@berkeley.edu>
2023-07-24 00:45:17 -0700

    Start of workflow to publish nightly releases

---
+++ .github/scripts/nightly-version.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Set a new version number based on the next minor version,
+# combined with a patch version based on the current date.
+npm version minor --no-git-tag-version > /dev/null 2>&1
+version=$(echo "$(npm pkg get version)" | tr -d '"')
+date=$(date +%Y%m%d)
+# NOTE: the following assumes that only simple
+# major.minor.patch version numbers are being used.
+nightly="${version%.*}.$date"
+npm version $nightly --no-git-tag-version
