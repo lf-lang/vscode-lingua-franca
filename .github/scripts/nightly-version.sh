a88a736a83b50b2fe1161bc3599dc39ac035dc42
Marten Lohstroh <marten@berkeley.edu>
2023-07-24 10:38:50 -0700

    Adjust working directory

--- .github/scripts/nightly-version.sh
+++ .github/scripts/nightly-version.sh
@@ -5,8 +5,8 @@
 npm version minor --no-git-tag-version > /dev/null 2>&1
 version=$(echo "$(npm pkg get version)" | tr -d '"')
 date=$(date +%Y%m%d)
+
 # NOTE: the following assumes that only simple
 # major.minor.patch version numbers are being used.
 nightly="${version%.*}.$date"
-echo $nightly
 npm version $nightly --no-git-tag-version
