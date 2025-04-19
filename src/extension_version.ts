5e09b0fd05a18f40af155b14d3bad677f5849db7
Edward A. Lee <eal@berkeley.edu>
2025-04-19 16:44:27 -0700

    Update version hash

--- src/extension_version.ts
+++ src/extension_version.ts
@@ -1,3 +1,3 @@
 'use strict';
 // This is a generated file. Do not edit.
-export const version = "3d655f812e25ca29012b614acdc91db0bf4fc29f";
+export const version = "06e31ca6b8b8ce98e04fc0861867f110c9b9f07b";
