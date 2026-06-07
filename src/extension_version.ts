fe071bbe54b26a149e786a4db180a76700b0eae2
Edward A. Lee <eal@berkeley.edu>
2026-06-07 06:16:04 -0700

    Update lingua-franca to get display of update_after

--- src/extension_version.ts
+++ src/extension_version.ts
@@ -1,3 +1,3 @@
 'use strict';
 // This is a generated file. Do not edit.
-export const version = "84bdd2de915c49aefc3e9eea57c4f9ede3da60b6";
+export const version = "9741dc57680b3c370d70fe7280c8de3f2105151a";
